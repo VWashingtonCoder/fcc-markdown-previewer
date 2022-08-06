0652e9f52281050c7bb83ed28d9b315933aceb2e
Vincent Washington <kuraiokamiblaze@gmail.com>
2022-08-06 15:34:58 -0600

    added in freecodecamp tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import Badge from "react-bootstrap/Badge"
+import { marked } from "marked";
 
-export default class App extends React.Component{
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: ""
+    };
+  };
+  
+  updateMarkdown(markdown) {
+    this.setState({ markdown });
+  };
+
   render() {
     var inputStyle = {
       width: "400px",
@@ -41,7 +53,16 @@ export default class App extends React.Component{
                   </Badge>
                 </h4>
                 <div className="mark-input">
-                  <textarea className="input" style={inputStyle}></textarea>
+                  <textarea 
+                    className="input" 
+                    style={inputStyle}
+                    value={this.state.markdown}
+                    onChange={(e) => {
+                      this.updateMarkdown(e.target.value)
+                    }}
+                  >
+                    {console.log(this.state.markdown)}
+                  </textarea>
                 </div>
             </div>
 
@@ -51,7 +72,10 @@ export default class App extends React.Component{
                     Markdown Preview
                   </Badge>
                 </h4>
-                <div style={outputStyle}></div>
+                <div 
+                  style={outputStyle} 
+                  dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+                ></div>
             </div>
           </div>
         </div>
