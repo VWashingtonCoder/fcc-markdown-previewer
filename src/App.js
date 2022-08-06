859586f91c015f2bcd36f0fb90c5a81c02ae70f4
Vincent Washington <kuraiokamiblaze@gmail.com>
2022-08-06 15:14:07 -0600

    markdown input added and styled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Badge from "react-bootstrap/Badge"
 
 export default class App extends React.Component{
   render() {
+    var inputStyle = {
+      width: "400px",
+      height: "50vh",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: "10px"
+    };
+
     return (
       <div className="App">
         <div className="container">
@@ -17,12 +25,15 @@ export default class App extends React.Component{
           </div>
                  
           <div className="row mt-4">
-            <div className="col-md-6">
-                <h4 className="text-center">
+            <div className="col-md-6 text-center">
+                <h4>
                   <Badge className="badge bg-secondary">
                     Markdown Input
                   </Badge>
                 </h4>
+                <div className="mark-input">
+                  <textarea className="input" style={inputStyle}></textarea>
+                </div>
             </div>
 
             <div className="col-md-6">
