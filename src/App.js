fd07718065e9701f7b9fe4eca9837babd0706244
Vincent Washington <kuraiokamiblaze@gmail.com>
2022-08-06 14:59:09 -0600

    finished the initial headings and subheadings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Badge from "react-bootstrap/Badge"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="badge bg-primary">
+                  Markdown Previewer
+                </Badge>
+              </h1>
+            </div>
+          </div>
+                 
+          <div className="row mt-4">
+            <div className="col-md-6">
+                <h4 className="text-center">
+                  <Badge className="badge bg-secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+            </div>
 
-export default App;
+            <div className="col-md-6">
+                <h4 className="text-center" >
+                  <Badge className="badge bg-secondary">
+                    Markdown Preview
+                  </Badge>
+                </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+};
