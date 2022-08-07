d7d73727d7a8783afd220d004e54a24ab728eece
Vincent Washington <kuraiokamiblaze@gmail.com>
2022-08-06 22:06:55 -0600

    updated App to pass tests

--- src/App.js
+++ src/App.js
@@ -2,11 +2,60 @@ import React from "react";
 import Badge from "react-bootstrap/Badge"
 import { marked } from "marked";
 
+marked.setOptions({
+  breaks: true,
+});
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: ""
+      markdown: placeholder  
     };
   };
   
@@ -24,8 +73,8 @@ export default class App extends React.Component {
     };
 
     var outputStyle = {
-      width: "400px",
-      height: "50vh",
+      width: "auto",
+      height: "auto",
       backgroundColor: "#DCDCDC",
       marginLeft: "auto",
       marginRight: "auto",
@@ -54,15 +103,14 @@ export default class App extends React.Component {
                 </h4>
                 <div className="mark-input">
                   <textarea 
+                    id="editor"
                     className="input" 
                     style={inputStyle}
                     value={this.state.markdown}
                     onChange={(e) => {
                       this.updateMarkdown(e.target.value)
                     }}
-                  >
-                    {console.log(this.state.markdown)}
-                  </textarea>
+                  ></textarea>
                 </div>
             </div>
 
@@ -73,6 +121,7 @@ export default class App extends React.Component {
                   </Badge>
                 </h4>
                 <div 
+                  id="preview"
                   style={outputStyle} 
                   dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
                 ></div>
